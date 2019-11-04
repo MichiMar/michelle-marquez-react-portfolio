7dea6c97dd7c8149660a8a9144a1fe7c775866f0
MichiMar <michimar.alexis@gmail.com>
2019-11-03 21:23:53 -0700

    update

--- src/components/Pages/about.js
+++ src/components/Pages/about.js
@@ -15,20 +15,15 @@ const About = () => {
       />
       <div className="rigth-column">
         <p>
-          Consequatur, deserunt rem. Quisquam, consequatur commodi quibusdam
-          soluta perspiciatis aliquam harum nobis porro quas rem explicabo
-          magni! Facilis, vel qui non ad cumque voluptatibus facere, dolores
-          atque veniam, reiciendis at. Quos voluptatibus, voluptates praesentium
-          earum quia corrupti ipsa delectus quibusdam aut ad sequi omnis placeat
-          libero labore magni officiis adipisci cupiditate tenetur obcaecati
-          tempora eaque modi nihil! Mollitia, dignissimos dolorum. Impedit
-          quaerat inventore temporibus perferendis ex adipisci molestias eos
-          deleniti nostrum, tenetur aliquid, iste fugiat, dolorum dignissimos
-          repellendus voluptates! Debitis, quae? Consequatur perspiciatis vel
-          dignissimos, totam fugit vero dolorum facilis. Illum, repellendus
-          dolore eum id cumque est iusto aut fugit. Molestias dolorem aliquid
-          cupiditate, alias explicabo magni, facere deserunt deleniti ad
-          voluptatem, soluta incidunt.
+          Born and Grew up in Mexico City, Mexico, I am proactive, resourceful
+          and always looking for ways to improve my work and the process I take
+          to achieve the desired goal. I have mostly worked in the area of sales
+          and customer service, so I have experience at work teams in small
+          stores and the responsive train new staff constantly. I love to do
+          amateur photography as a hobby and spend time with my family. One of
+          my biggest dreams is to workin educational websites and bring new and
+          incredible knowledge in schools, available for students and teachers
+          so they can grew up together as a team.
         </p>
       </div>
     </div>
