99f57c8f7be63860bd2e5c6c75c3b67009253dea
MichiMar <michimar.alexis@gmail.com>
2020-05-15 23:39:24 -0500

    date format

--- src/components/app.js
+++ src/components/app.js
@@ -92,6 +92,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="container">
+        {/* date format in the top */}
         <div>{moment().format("MMMM Do YY")}</div>
 
         <Router>
